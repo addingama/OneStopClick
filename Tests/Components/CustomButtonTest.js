80ad637dd42113e8bf64d0fc5fb5efee410b80a9
Addin Gama Bertaqwa <punyagama@yahoo.com>
2017-08-08 10:55:37 +0700

    20170808 - Unit Test - Add unit test for HamburgerMenu and CustomButton Component

---
+++ Tests/Components/CustomButtonTest.js
@@ -0,0 +1,9 @@
+import 'react-native'
+import React from 'react'
+import { CustomButton } from '../../App/Components/FormGenerator'
+import renderer from 'react-test-renderer'
+
+test('CustomButton component renders correctly if title prop set', () => {
+  const tree = renderer.create(<CustomButton  title='new button'/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
