b87d471df801166bac10b6f699d4d8e8f606ab60
Addin Gama Bertaqwa <punyagama@gmail.com>
2017-08-30 05:38:13 +0700

    20170830 - General - Add push notification integration to mobile.azure.com

--- android/app/src/main/java/com/onestopclick/MainApplication.java
+++ android/app/src/main/java/com/onestopclick/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.push.RNPushPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPushPackage(MainApplication.this),
             new VectorIconsPackage(),
             new RNDeviceInfo(),
             new ReactNativeConfigPackage(),
