d2c532f0ec04a594b6d086e73da1381dbec708ab
Addin Gama Bertaqwa <punyagama@gmail.com>
2017-08-31 13:36:30 +0700

    20170830 - Cart - Generate Cart Button Component

---
+++ App/Components/CartButton.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types';
+import { View, Text, TouchableWithoutFeedback } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { HideableView } from 'react-native-hideable-view'
+import IconBadge from 'react-native-icon-badge'
+import styles from './Styles/CartButtonStyle'
+
+export default class CartButton extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      BadgeCount: 0
+    }
+  }
+
+  componentWillReceiveProps (newProps) {
+    this.setState({
+      BadgeCount: newProps.BadgeCount
+    })
+  }
+
+  renderBadge (hasBadge) {
+    if (hasBadge) {
+      return (
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeCount}>
+            {this.state.BadgeCount}
+          </Text>
+        </View>
+      )
+    }
+  }
+
+  render () {
+    return (
+      <View>
+        <TouchableWithoutFeedback>
+          <View style={{ flexDirection: 'row' }}>
+            <Icon name='shopping-cart' color='white' />
+            { this.renderBadge(this.state.BadgeCount !== 0)}
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
+  }
+}
---
+++ App/Components/Styles/CartButtonStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  badgeContainer: {
+    backgroundColor: 'red',
+    marginTop: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'red',
+    overflow: 'hidden'
+  },
+  badgeCount: {
+    color: '#FFFFFF',
+    fontSize: 11
+  }
+
+})
